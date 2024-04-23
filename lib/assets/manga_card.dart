0096c8c3001a4d8b0f83edc8889f3eea1f3fbdf0
Jonathan <107511723+thanrih@users.noreply.github.com>
2024-04-23 14:32:36 -0300

    Update README.md
    alteração do readme

---
+++ lib/assets/manga_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MangaCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+  final String badge;
+
+  const MangaCard({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    this.subtitle = '',
+    this.badge = '',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+            height: 200,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                if (subtitle.isNotEmpty)
+                  Text(
+                    subtitle,
+                    style: const TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 16,
+                    ),
+                  ),
+                if (badge.isNotEmpty)
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      badge,
+                      style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import '../assets/manga_card.dart';
 
 class MyHomePage extends StatelessWidget {
   final String title;
@@ -7,14 +9,42 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> urls = ['https://image.api.playstation.com/vulcan/ap/rnd/202008/0611/VdFDkKKhAXpyw5NLBgfQSaW9.jpg'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-
-        child: Text('Conteúdo da página'),
-
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: [
+              ...List.generate(
+                3,
+                    (index) => Container(
+                  child: MangaCard(
+                    imageUrl: 'https://m.media-amazon.com/images/I/81sphDUj6aS._AC_UF1000,1000_QL80_.jpg',
+                    title: 'Frieren',
+                    subtitle: 'A maginha fofa',
+                  ),
+                ),
+              ),
+            ],
+            options: CarouselOptions(
+              height: 300,
+              viewportFraction: 0.8,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+        ],
       ),
     );
   }
