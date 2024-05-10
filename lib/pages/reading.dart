2691c3b98e532df33dd06fc45fa7241d39c1e0ec
Thanrih <jonathanrodriguesdosreis1@gmail.com>
2024-05-10 17:14:58 -0300

    Página reading.dart agora pode rolar.

--- lib/pages/reading.dart
+++ lib/pages/reading.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class ReadingPage extends StatelessWidget {
-
   const ReadingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Reading'),
       ),
-      body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-
-          Center(child: Image.network('https://cdn.hxmanga.com/file/majekayoo/solo-leveling/Vol.2-Chapter-142/1.jpg')),
-        ],
+      body: SingleChildScrollView( // Wrap the content with SingleChildScrollView
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.network(
+                'https://cdn.hxmanga.com/file/majekayoo/solo-leveling/Vol.2-Chapter-142/1.jpg',
+                fit: BoxFit.fill, // Adjust image size as needed
+              ),
+            ),
+          ],
+        ),
       ),
     );
-
   }
 }
